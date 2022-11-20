eb93cfd04f76284c0d1dd17808409b4a55b3bc53
Ricky Aguilar <r3aguila@ucsd.edu>
2022-11-20 14:17:32 -0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 8 - Starter
+Ricardo Aguilar
+1. 1 because that would trigger the test that is associated with the issue to be ran.
+2. No
+3. 
--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -17,27 +17,32 @@ describe('Basic user flow for Website', () => {
 
   // Check to make sure that all 20 <product-item> elements have data in them
   it('Make sure <product-item> elements are populated', async () => {
-    console.log('Checking to make sure <product-item> elements are populated...');
-    // Start as true, if any don't have data, swap to false
-    let allArePopulated = true;
-    let data, plainValue;
-    // Query select all of the <product-item> elements
-    const prodItems = await page.$$('product-item');
-    console.log(`Checking product item 1/${prodItems.length}`);
-    // Grab the .data property of <product-items> to grab all of the json data stored inside
-    data = await prodItems[0].getProperty('data');
-    // Convert that property to JSON
-    plainValue = await data.jsonValue();
-    // Make sure the title, price, and image are populated in the JSON
-    if (plainValue.title.length == 0) { allArePopulated = false; }
-    if (plainValue.price.length == 0) { allArePopulated = false; }
-    if (plainValue.image.length == 0) { allArePopulated = false; }
-    // Expect allArePopulated to still be true
-    expect(allArePopulated).toBe(true);
+    while(prodItems != null){
+      console.log('Checking to make sure <product-item> elements are populated...');
+      // Start as true, if any don't have data, swap to false
+      let allArePopulated = true;
+      let data, plainValue;
+      // Query select all of the <product-item> elements
+      const prodItems = await page.$$('product-item');
+      console.log(`Checking product item 1/${prodItems.length}`);
+      // Grab the .data property of <product-items> to grab all of the json data stored inside
+      data = await prodItems[0].getProperty('data');
+      // Convert that property to JSON
+      plainValue = await data.jsonValue();
+      // Make sure the title, price, and image are populated in the JSON
+      if (plainValue.title.length == 0) { allArePopulated = false; }
+      if (plainValue.price.length == 0) { allArePopulated = false; }
+      if (plainValue.image.length == 0) { allArePopulated = false; }
+      // Expect allArePopulated to still be true
+      expect(allArePopulated).toBe(true);
 
-    // TODO - Step 1
-    // Right now this function is only checking the first <product-item> it found, make it so that
-    // it checks every <product-item> it found
+      // TODO - Step 1
+      // Right now this function is only checking the first <product-item> it found, make it so that
+      // it checks every <product-item> it found
+      /* I just added a While loop*/
+
+    }
+
 
   }, 10000);
 
@@ -50,6 +55,9 @@ describe('Basic user flow for Website', () => {
     // Grab the shadowRoot of that element (it's a property), then query a button from that shadowRoot.
     // Once you have the button, you can click it and check the innerText property of the button.
     // Once you have the innerText property, use innerText.jsonValue() to get the text value of it
+    const prodItems = await page.$$('product-item');
+    
+
   }, 2500);
 
   // Check to make sure that after clicking "Add to Cart" on every <product-item> that the Cart
